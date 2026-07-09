d266f37cf515935682ed369e1a334143cf1b9821
RyanKirkland07 <kryan8574@gmail.com>
2026-07-09 04:22:54 +0000

    Added the function to loop through library and display books in HTML page

--- library.js
+++ library.js
@@ -10,12 +10,44 @@ function Book(title, author, pages, read, id){
 }
 
 function addBookToLibrary(title, author, pages, read){
-    let newBook = new Book(title, author, pages, read, crypto.randomUUID);
+    let newBook = new Book(title, author, pages, read, crypto.randomUUID());
     myLibrary.push(newBook);
 }
 
 function displayLibrary(){
     for(book of myLibrary){
-        bookCard = document.createElement("div");
+        let bookCard = document.createElement("div");
+        bookCard.className = "book-card";
+
+        let text = document.createElement("h1")
+        text.textContent = book.title;
+        bookCard.appendChild(text);
+
+        text = document.createElement("h2");
+        text.textContent = "by, " + book.author;
+        bookCard.appendChild(text);
+
+        text = document.createElement("p");
+        text.textContent = book.pages + " pages.";
+        bookCard.appendChild(text);
+
+        text = document.createElement("p");
+        text.textContent = "Read?: ";
+        if(book.read){
+            text.textContent += "✔️";
+        }
+        else{
+            text.textContent += "❌";
+        }
+        bookCard.appendChild(text);
+
+        text = document.createElement("p");
+        text.textContent = "UUID: " + book.id;
+        bookCard.appendChild(text);
+
+        document.body.appendChild(bookCard);
     }
 }
+
+addBookToLibrary("book title", "ryan", 999, true);
+displayLibrary();
